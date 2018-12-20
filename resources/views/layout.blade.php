3e7058785887055762583c301782d7488cbddcbe
proxyht <caohoangtung2001@gmail.com>
2018-12-20 16:06:51 +0700

    fix https file serving

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,15 +10,15 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/jquery-3.1.0.min.js') }}"></script>
+    <script src="/js/app.js'" defer></script>
+    <script src="/js/jquery-3.1.0.min.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" defer>
-    <link href="{{ asset('css/customlayout.css') }}" rel="stylesheet" defer>
+    <link href="/css/app.css" rel="stylesheet" defer>
+    <link href="/css/customlayout.css" rel="stylesheet" defer>
 </head>
 <body>
     <div id="app">
